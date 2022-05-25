651851581a1c59ab604790ce709629652344667a
Alexandra Oliveira Goncalves <alexandragoncalves6970@gmail.com>
2022-05-25 16:19:28 +0200

    License added throught bat file

--- create_project.bat
+++ create_project.bat
@@ -5,7 +5,7 @@
 
 
 ::create new folder based on input name
-echo What do you want the folder to be called?
+echo "What do you want the folder to be called?"
 set /P folderName=
 md "%CD%\%folderName%"
 
@@ -17,4 +17,10 @@ md "scripts"
 :: copy paste the readme file into the project file
 xcopy "C:\Users\alexa\Desktop\Studies\Perso\Project_automation\README.md" "%CD%\"
 
+:: copy paste the license file into the project file
+echo "Do you want to add MIT License ? [Y/N]"
+set /P userChoice=
+if /I "%UserChoice%" == "Y" (xcopy "C:\Users\alexa\Desktop\Studies\Perso\Project_automation\LICENSE" "%CD%\")
+
+echo "Done"
 ::python C:\Users\alexa\Desktop\Studies\Perso\Project_automation\project_auto.py
